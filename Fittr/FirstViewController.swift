e131b8ea057dedef5a3d5f6a29768949f35db0d3
Adrit Lath <adritlath@gmail.com>
2015-01-21 22:58:00 -0800

    Initial Commit

---
+++ Fittr/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fittr
+//
+//  Created by Adrit Lath on 1/21/15.
+//  Copyright (c) 2015 Adrit Lath. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
